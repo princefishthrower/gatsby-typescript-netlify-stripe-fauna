4cf2a0984cb00bc4eb5aa316d87635e4762e3b0a
Chris Frewin <frewin.christopher@gmail.com>
2021-01-29 12:33:56 +0100

    damn environment vars

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,17 +1,17 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { faunaFetch } = require('./utils/fauna');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const { faunaFetch } = require('./utils/fauna')
 
-exports.handler = async (event) => {
-  const { user } = JSON.parse(event.body);
+exports.handler = async event => {
+  const { user } = JSON.parse(event.body)
 
   // create a new customer in Stripe
-  const customer = await stripe.customers.create({ email: user.email });
+  const customer = await stripe.customers.create({ email: user.email })
 
   // subscribe the new customer to the free plan
   await stripe.subscriptions.create({
     customer: customer.id,
-    items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
-  });
+    items: [{ price: process.env.STRIPE_FREE_PRICE_ID }]
+  })
 
   // store the Netlify and Stripe IDs in Fauna
   await faunaFetch({
@@ -25,16 +25,16 @@ exports.handler = async (event) => {
     `,
     variables: {
       netlifyID: user.id,
-      stripeID: customer.id,
-    },
-  });
+      stripeID: customer.id
+    }
+  })
 
   return {
     statusCode: 200,
     body: JSON.stringify({
       app_metadata: {
-        roles: ['free'],
-      },
-    }),
-  };
-};
+        roles: ['free']
+      }
+    })
+  }
+}
